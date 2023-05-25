85e62f948b626461bcfe3a45ca94663f3ac7cadb
Ravele Silva <ravele.cs@gmail.com>
2023-05-25 18:45:46 -0300

    Exercitando funcionalidade

--- navegacaoTab.js
+++ navegacaoTab.js
@@ -1,19 +1,20 @@
-document.documentElement.classList.add('js')
-
 const imagesList = document.querySelectorAll('.js-tabmenu li')
-const animalsDescriptions = document.querySelectorAll('.js-tabcontent .animal-desc')
+const animalsContent = document.querySelectorAll('.js-tabcontent .animal-desc')
 
-if (animalsDescriptions && imagesList){
-  function activeTab(index){
-    animalsDescriptions.forEach((item)=>{
-      item.classList.remove('ativo')
-    })
-    animalsDescriptions[index].classList.add('ativo')
-  }
+//Função para remover a classe 'ativo de todos os itens de animalsContent e adicionar apenas no item com o índice do parâmetro
 
-  imagesList.forEach((item, index)=>{
-    item.addEventListener('click', function(){
-      activeTab(index)
-    })
+function activeTab(index){
+  animalsContent.forEach((item)=>{
+    item.classList.remove('ativo')
   })
+
+  animalsContent[index].classList.add('ativo')
 }
+
+//Função para adicionar o evento de click em todos os elementos de imagesList e ativar a função activeTab com o índice do elemento
+
+imagesList.forEach((item, index)=>{
+  item.addEventListener('click', function(){
+    activeTab(index)
+  })
+})

07507634fb8efc5fb19e5d2994b058a051260f0a
Ravele Silva <ravele.cs@gmail.com>
2023-06-21 18:32:47 -0300

    Feature de tooltip no mapa

---
+++ JavaScript/Modules/tooltip.js
@@ -0,0 +1,43 @@
+export default function initTooltip(){}
+
+const toolTips = document.querySelectorAll('[data-tooltip]')
+
+toolTips.forEach((item)=>{
+  item.addEventListener('mouseover', onMouseOver)
+})
+
+function onMouseOver(){
+  const tooltipBox = criarTooltipBox(this)
+
+  onMouseMove.tooltipBox = tooltipBox
+  onMouseLeave.tooltipBox = tooltipBox
+  onMouseLeave.element = this
+
+  this.addEventListener('mouseleave', onMouseLeave)
+  this.addEventListener('mousemove', onMouseMove)
+}
+
+const onMouseLeave = {
+  handleEvent(){
+    this.tooltipBox.remove()
+    this.element.removeEventListener('mouseleave', onMouseLeave)
+    this.element.removeEventListener('mousemove', onMouseMove)
+  }
+}
+
+const onMouseMove = {
+  handleEvent(event){
+    this.tooltipBox.style.top = event.pageY + 20 + 'px'
+    this.tooltipBox.style.left = event.pageX + 20 + 'px'
+  }
+}
+
+
+function criarTooltipBox(element){
+  const tooltipBox = document.createElement('div')
+  const tooltipText = element.getAttribute('aria-label')
+  tooltipBox.classList.add('tooltip')
+  tooltipBox.innerText = tooltipText
+  document.body.appendChild(tooltipBox)
+  return tooltipBox
+}
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,8 +2,10 @@ import tabNav from "./Modules/tabNav.js";
 import initAnimaScroll from "./Modules/anima-scroll.js";
 import initAccordion from "./Modules/accordion.js";
 import initModal from "./Modules/modal.js";
+import initTooltip from "./Modules/tooltip.js";
 
 initModal()
 initAnimaScroll()
 tabNav()
 initAccordion()
+initTooltip()

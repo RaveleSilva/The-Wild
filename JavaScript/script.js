9af524f01a535a691acb79c41639417a414f733b
Ravele Silva <ravele.cs@gmail.com>
2023-09-06 15:13:52 -0300

    Accordion refatorado

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,6 @@
 import tabNav from './Modules/tabNav.js';
 import initAnimaScroll from './Modules/anima-scroll.js';
-import initAccordion from './Modules/accordion.js';
+import Accordion from './Modules/accordion.js';
 import initModal from './Modules/modal.js';
 import initTooltip from './Modules/tooltip.js';
 import initDropDownMenu from './Modules/dropDownMenu.js';
@@ -9,12 +9,14 @@ import initFuncionamento from './Modules/funcionamento.js';
 import initFetchAnimais from './Modules/fetchAnimais.js';
 import initFetchBitcoin from './Modules/fetchBitcoin.js';
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initModal();
 initMenuMobile();
 initDropDownMenu();
 initAnimaScroll();
 tabNav();
-initAccordion();
 initTooltip();
 initFuncionamento();
 initFetchAnimais();

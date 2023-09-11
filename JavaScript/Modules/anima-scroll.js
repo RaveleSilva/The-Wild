1be73af7f4e52bcec055ef9b752125e8d86fb262
Ravele Silva <ravele.cs@gmail.com>
2023-09-11 17:05:45 -0300

    AnimaScroll otimizado

--- JavaScript/Modules/anima-scroll.js
+++ JavaScript/Modules/anima-scroll.js
@@ -3,24 +3,38 @@ export default class AnimaScroll {
     this.sections = document.querySelectorAll(sections);
     this.windowSize = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowSize) < 0;
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowSize),
+      };
+    });
+  }
 
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
